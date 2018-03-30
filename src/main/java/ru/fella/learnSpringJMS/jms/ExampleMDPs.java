3a661998d0cf4091ed3d74dd106b294b7390008a
Ella Fischenko <efischenko@cinimex.ru>
2018-03-30 12:20:41 +0300

    Add Message-Driven POJOs

---
+++ src/main/java/ru/fella/learnSpringJMS/jms/ExampleMDPs.java
@@ -0,0 +1,19 @@
+package ru.fella.learnSpringJMS.jms;
+
+import fella.ru.Person;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by efischenko on 30.03.2018.
+ */
+//This class named in Spring Doc ->  Asynchronous reception: Message-Driven POJOs
+@Component
+public class ExampleMDPs {
+
+    // Annotation-driven listener endpoints
+    @JmsListener(destination = "${queu.name}")
+    public void onMessage(Person person) {
+        System.out.println("received " + person);
+    }
+}
